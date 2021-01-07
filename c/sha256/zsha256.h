4f83abf31ec9e1d3754ab6872356279a3a4907b2
Ralph Wang <ralph.wang@advantech.com.tw>
2021-01-07 16:39:14 +0800

    add sha256 sample

---
+++ c/sha256/zsha256.h
@@ -0,0 +1,19 @@
+#ifndef _M_ZSHA256_H
+#define _M_ZSHA256_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <memory.h>
+#include <string.h>
+
+#ifdef  __cplusplus  
+extern "C" {  
+#endif 
+
+int zsha256(const uint8_t *src, uint32_t len, uint32_t *hash);
+
+#ifdef  __cplusplus  
+}
+#endif 
+#endif
